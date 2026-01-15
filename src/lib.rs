2340e3457c1d2a64215c39bf8da29a5f787ccfac
isaac3102 <isaaclee3110@gmail.com>
2026-01-15 17:44:56 +0800

    fixed some stuff

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,31 @@ use anyhow::{anyhow, Result};
 use std::sync::Arc;
 use std::collections::HashMap;
 
-use crate::models::{Character, Placement};
-mod models; 
+#[derive(Copy, Clone)]
+pub enum Placement {
+    Absent, // Not in the word
+    Present, // In the word but wrong position
+    Correct, // In the correct position
+    Unknown, // Not yet analyzed
+}
+
+#[derive(Copy, Clone)]
+pub struct Character {
+    character: char,
+    placement: Placement,
+    index: usize,
+}
+
+impl Character {
+    pub fn new(character: char, placement: Placement, index: usize) -> Self {
+        Character { character, placement, index }
+    }
+
+    fn update_placement(&mut self, new_placement: Placement, new_index: usize) {
+        self.placement = new_placement;
+        self.index = new_index;
+    }
+}
 
 /* Usage of lib.rs
 
